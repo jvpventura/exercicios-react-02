de810877162d091e65fd024c2061e06b520bd100
jvpventura <jvventura.br@gmail.com>
2026-02-18 17:02:59 -0300

    Corrigido alinhamento da página User

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -8,9 +8,11 @@ function User() {
       <h2>Perfil do Usuário</h2>
       <p>ID do usuário: {id}</p>
 
-      <Link to="/" className="btn">
-        Voltar para pagina inicial
-      </Link>
+      <div className="page-links">
+        <Link to="/" className="btn">
+          Voltar para pagina inicial
+        </Link>
+      </div>
     </div>
   );
 }
